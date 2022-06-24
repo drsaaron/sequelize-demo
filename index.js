cd0822e5ba78d3e5ad82e130891ae4ea6615deb9
aar1069 <scottaaron@northwesternmutual.com>
2022-06-24 14:57:25 -0500

    add item create, item delete, and query where aspects

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ const Person = sequelize.define('Person', {
     // model attributes
     personId: {
 	type: DataTypes.INTEGER,
-	allowNull: false,
-	primaryKey: true
+	allowNull: true,
+	primaryKey: true,
+	autoincrement: true
     },
     
     name: {
@@ -38,14 +39,39 @@ const Person = sequelize.define('Person', {
     timestamps: false
 });
 
+async function logAll() {
+    // query all
+    const people = await Person.findAll();
+    console.log(people.every(user => user instanceof Person)); // true
+    console.log("All people:", JSON.stringify(people, null, 2));
+}
+
+async function logKlingons() {
+    const klingons = await Person.findAll({ where: { personTypCde: 4 }});
+    console.log("klingons:", JSON.stringify(klingons, null, 2));
+}
+
 async function main() {
     // the defined model is the class itself
     console.log(Person === sequelize.models.Person); // true
     
     // query all
-    const people = await Person.findAll();
-    console.log(people.every(user => user instanceof Person)); // true
-    console.log("All people:", JSON.stringify(people, null, 2));
+    await logAll();
+
+    // add a new user.
+    const page = await Person.create({ name: 'Jimmy Page', personTypCde: 3 });
+    await logAll();
+
+    // delete the Jimmy Page's
+    await Person.destroy({
+	where: {
+	    name: 'Jimmy Page'
+	}
+    });
+    await logAll();
+
+    // just query klingons
+    await logKlingons();
 };
 
 main();
